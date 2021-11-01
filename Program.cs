f3ce81b0fa106dde6d2fe971546374ded78e7d54
Jacob <frizjr@protonmail.com>
2021-11-01 13:29:46 +0200

    Added ReadMe.md. Created GetInfoFromConsole method for storing user input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleUI
 {
@@ -6,7 +7,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            List<string> listOfParties = new List<string>();
+            string moreGuestsComing = "";
+            
+            do
+            {
+                listOfParties.Add(GetInfoFromConsole("What is the full name of the main guest of this party?"));
+                moreGuestsComing = GetInfoFromConsole("Will there be more guests coming to register (yes/no)?");
+            } while (moreGuestsComing.ToLower() == "yes");
+            
             Console.ReadLine();
+        }
+
+        private static string GetInfoFromConsole(string message)
+        {
+            Console.WriteLine(message);
+                string output = Console.ReadLine();
+            return output;
         }
     }
 }
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+## Console Guest List app
+
+Asks the user to input the lead guest's name.
+Commits that name to a list of guest names.
+
+Asks the user to input the lead guest's party count.
+Adds that party count to the counter of total guests.
+
+Prints all lead guest names. 
+Prints total number of party-goers.
+
